9af0071ccc30af14a53488fe09bd678e86e150c8
llc1234 <93080319+llc1234@users.noreply.github.com>
2025-05-04 05:15:43 +0200

    Add files via upload

---
+++ python/Trangle.py
@@ -0,0 +1,93 @@
+import glfw
+from OpenGL.GL import *
+import numpy as np
+
+
+
+
+VERTEX_SHADER_SOURCE = """
+#version 330 core
+layout(location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
+}
+"""
+
+FRAGMENT_SHADER_SOURCE = """
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(0.2, 0.6, 1.0, 1.0);
+}
+"""
+
+
+glfw.init()
+window = glfw.create_window(800, 600, "TRIANGLE", None, None)
+glfw.make_context_current(window)
+
+
+def compile_shader(source, shader_type):
+    shader = glCreateShader(shader_type)
+    glShaderSource(shader, source)
+    glCompileShader(shader)
+    if glGetShaderiv(shader, GL_COMPILE_STATUS) != GL_TRUE:
+        raise RuntimeError(glGetShaderInfoLog(shader))
+    return shader
+
+vertex_shader = compile_shader(VERTEX_SHADER_SOURCE, GL_VERTEX_SHADER)
+fragment_shader = compile_shader(FRAGMENT_SHADER_SOURCE, GL_FRAGMENT_SHADER)
+
+shader_program = glCreateProgram()
+
+glAttachShader(shader_program, vertex_shader)
+glAttachShader(shader_program, fragment_shader)
+
+glLinkProgram(shader_program)
+
+glDeleteShader(vertex_shader)
+glDeleteShader(fragment_shader)
+
+vertices = np.array([
+     0.0,  0.5, 0.0,
+    -0.5, -0.5, 0.0,
+     0.5, -0.5, 0.0
+], dtype=np.float32)
+
+VBO = glGenBuffers(1)
+VAO = glGenVertexArrays(1)
+
+glBindVertexArray(VAO)
+glBindBuffer(GL_ARRAY_BUFFER, VBO)
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * vertices.itemsize, ctypes.c_void_p(0))
+glEnableVertexAttribArray(0)
+
+glBindBuffer(GL_ARRAY_BUFFER, 0)
+glBindVertexArray(0)
+
+
+while not glfw.window_should_close(window):
+    glfw.poll_events()
+
+    glClearColor(0.1, 0.1, 0.1, 1.0)
+    glClear(GL_COLOR_BUFFER_BIT)
+
+
+    glUseProgram(shader_program)
+
+    glBindVertexArray(VAO)
+    glDrawArrays(GL_TRIANGLES, 0, len(vertices))
+
+    glfw.swap_interval(0)
+    glfw.swap_buffers(window)
+
+glDeleteVertexArrays(1, [VAO])
+glDeleteBuffers(1, [VBO])
+glDeleteProgram(shader_program)
+glfw.terminate()
